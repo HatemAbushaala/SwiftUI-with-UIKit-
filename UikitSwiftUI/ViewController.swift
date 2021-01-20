42c32d2904f0b92170e88ae41afa140d364da29b
h.abushaala@yahoo.com <0776213065Hatem>
2021-01-20 11:27:44 +0300

    open basic view without passing data

--- UikitSwiftUI/ViewController.swift
+++ UikitSwiftUI/ViewController.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let page = UIHostingController(rootView:Page() )
+   
+        //navigationController?.pushViewController(page, animated: true)
+        present(page, animated: true, completion: nil)
     }
 
 
 }
 
+struct Page : View {
+    var body : some View{
+        Text("hello")
+    }
+}
