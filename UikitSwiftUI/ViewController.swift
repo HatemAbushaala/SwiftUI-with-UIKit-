5924d614d27d2716fd16bac9fd0682b8673bcbc2
h.abushaala@yahoo.com <0776213065Hatem>
2021-01-20 16:18:35 +0300

    open view controller from swift ui view and return result from view controller using UIViewControllerRepresentable

--- UikitSwiftUI/ViewController.swift
+++ UikitSwiftUI/ViewController.swift
@@ -26,26 +26,33 @@ class ViewController: UIViewController {
 }
 
 class ViewController2: UIViewController {
-    
+    var onResult : ((String)->Void)? = nil
+
     override func viewDidLoad() {
         super.viewDidLoad()
-       
         view.backgroundColor = .red
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.onResult?("data from view controller received")
+        }
     }
-    
-    
 }
 
 // bridge between uikit view controller and swift ui view
 struct VC2View : UIViewControllerRepresentable {
-    
+    var onResult : ((String)->Void)? = nil
+
     func makeUIViewController(context: Context) -> ViewController2 {
-        return ViewController2()
-    }
-    
-    func updateUIViewController(_ uiViewController: ViewController2, context: Context) {
+        let vc = ViewController2()
+        vc.onResult = { string in
+            // do something
+            self.onResult?(string)
+        }
+        return vc
         
     }
+    func updateUIViewController(_ uiViewController: ViewController2, context: Context) {}
+    
 }
 
 struct Page : View {
@@ -55,15 +62,15 @@ struct Page : View {
 
     var body : some View{
         
-        Button("click me"){
-            showSheet = !showSheet
-        }
+        Button("click me"){showSheet = !showSheet}
         
-        .sheet(isPresented: $showSheet) {
-            VC2View()
-        }
-        
-        
-        
+            .sheet(isPresented: $showSheet) {
+                VC2View(){ result in
+                    
+                    print("result is \(result)")
+                    showSheet = false
+                }
+                
+            }
     }
 }
